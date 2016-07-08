556c100ce24583b76dc7bbf39d75e1eeb7db1ccd
Kevin Scott <kevin@scottlabsllc.com>
2016-07-08 13:15:28 -0400

    Refactoring

---
+++ README.md
@@ -0,0 +1,3 @@
+# Redux Starter Kit
+
+
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        I am the App !!!!2
+        App 3
       </div>
     );
   }
