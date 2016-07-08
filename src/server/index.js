b90a321009ceb408271140cc40ce54795733d53f
Kevin Scott <kevin@scottlabsllc.com>
2016-07-08 17:20:05 -0400

    import bootstrap the es6 way

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,12 @@
 import path from 'path';
 import tool from 'webpack-isomorphic-tools';
 import toolConfig from '../../webpack/webpack-isomorphic-tools';
+import bootstrap from './bootstrap';
 
 const rootDir = path.resolve(__dirname, '../..');
 
 const isomorphicTools = new tool(toolConfig)
 .development(__DEVELOPMENT__)
 .server(rootDir, () => {
-  require('./bootstrap')(isomorphicTools);
+  bootstrap(isomorphicTools);
 });
